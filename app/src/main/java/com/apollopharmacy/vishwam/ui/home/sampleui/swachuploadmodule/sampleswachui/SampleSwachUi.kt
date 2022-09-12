82c7335647d51d475c2e424cb596fe7294965d2d
naveen <android@thresholdsoft.com>
2022-09-12 19:46:31 +0530

    .

--- app/src/main/java/com/apollopharmacy/vishwam/ui/home/sampleui/swachuploadmodule/sampleswachui/SampleSwachUi.kt
+++ app/src/main/java/com/apollopharmacy/vishwam/ui/home/sampleui/swachuploadmodule/sampleswachui/SampleSwachUi.kt
@@ -161,9 +161,9 @@ class SampleSwachUi : BaseFragment<SampleSwachViewModel, FragmentSampleuiSwachBi
             if (it != null) {
                 val sdf = SimpleDateFormat("EEEE")
                 val d = Date()
-                it.thursday = false
-                it.friday = false
-                it.thursday = true
+//                it.thursday = false
+//                it.friday = false
+//                it.thursday = true
                 val dayOfTheWeek: String = sdf.format(d)
                 charArray.add(it.sunday.toString())
                 charArray.add(it.monday.toString())
