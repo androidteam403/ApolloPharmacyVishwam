e9145ed3fa954e25addbfdb474aaf6a05b3fe747
pc <test@gmail.com>
2023-03-16 19:13:44 +0530

    new drug pull

--- app/src/main/java/com/apollopharmacy/vishwam/ui/home/MainActivity.java
+++ app/src/main/java/com/apollopharmacy/vishwam/ui/home/MainActivity.java
@@ -1291,13 +1291,13 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         listView.addHeaderModel(new HeaderModel("Cash Deposit", Color.WHITE, false, R.drawable.ic_apollo_pending));
 
-        if (isAttendanceRequired) {
+//        if (isAttendanceRequired) {
             listView.addHeaderModel(
                     new HeaderModel("Attendance Management", Color.WHITE, true, R.drawable.ic_menu_cms)
                             .addChildModel(new ChildModel("Attendance", R.drawable.ic_menu_reports))
                             .addChildModel(new ChildModel("History", R.drawable.ic_menu_survey))
             );
-        }
+//        }
         if (isCMSRequired) {
             listView.addHeaderModel(
                     new HeaderModel("CMS", Color.WHITE, true, R.drawable.ic_menu_cms)
