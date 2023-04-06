41b666a73f165862246898eb32ef2b6cd750d24d
pc <androidteam403>
2023-04-06 15:18:47 +0530

    .

--- app/src/main/java/com/apollopharmacy/vishwam/ui/home/MainActivity.java
+++ app/src/main/java/com/apollopharmacy/vishwam/ui/home/MainActivity.java
@@ -1350,32 +1350,32 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                         .addChildModel(new ChildModel("List", R.drawable.ic_apollo_list2)));
             }
         }
-//        if (true) {
-//
-//            if (getSessionManager().getLoginToken().isEmpty()) {
-//                listView.addHeaderModel(new HeaderModel("Vivekagam", Color.WHITE, true, R.drawable.ic_untitled_1)
-//                        .addChildModel(new ChildModel("Login", R.drawable.ic_apollo_pending)));
-//            } else if (getSessionManager().getLoginToken().length() > 1) {
-//                listView.addHeaderModel(new HeaderModel("Vivekagam", Color.WHITE, true, R.drawable.ic_untitled_1)
-//
-//                        .addChildModel(new ChildModel("Dashboard", R.drawable.ic_apollo_dashboard))
-//
-//                        .addChildModel(new ChildModel("My Orders", R.drawable.ic_apollo_list2))
-//                        .addChildModel(new ChildModel("Cash Deposits", R.drawable.ic_apollo_bill))
-//                        .addChildModel(new ChildModel("Summary", R.drawable.ic_apollo_survey_68__1_))
-//                        .addChildModel(new ChildModel("Complaints", R.drawable.ic_apollo_complaint_list))
-//
-//                        .addChildModel(new ChildModel("Profile", R.drawable.ic_apollo_survey_admin))
-//
-//                        .addChildModel(new ChildModel("Change Password", R.drawable.ic_apollo_complaint_register))
-//                        .addChildModel(new ChildModel("Help", R.drawable.ic_apollo_new_drug_request__1_))
-//                        .addChildModel(new ChildModel("LogOut", R.drawable.ic_apollo_pending))
-//
-//
-//                );
-//            }
-//
-//        }
+        if (true) {
+
+            if (getSessionManager().getLoginToken().isEmpty()) {
+                listView.addHeaderModel(new HeaderModel("Vivekagam", Color.WHITE, true, R.drawable.ic_untitled_1)
+                        .addChildModel(new ChildModel("Login", R.drawable.ic_apollo_pending)));
+            } else if (getSessionManager().getLoginToken().length() > 1) {
+                listView.addHeaderModel(new HeaderModel("Vivekagam", Color.WHITE, true, R.drawable.ic_untitled_1)
+
+                        .addChildModel(new ChildModel("Dashboard", R.drawable.ic_apollo_dashboard))
+
+                        .addChildModel(new ChildModel("My Orders", R.drawable.ic_apollo_list2))
+                        .addChildModel(new ChildModel("Cash Deposits", R.drawable.ic_apollo_bill))
+                        .addChildModel(new ChildModel("Summary", R.drawable.ic_apollo_survey_68__1_))
+                        .addChildModel(new ChildModel("Complaints", R.drawable.ic_apollo_complaint_list))
+
+                        .addChildModel(new ChildModel("Profile", R.drawable.ic_apollo_survey_admin))
+
+                        .addChildModel(new ChildModel("Change Password", R.drawable.ic_apollo_complaint_register))
+                        .addChildModel(new ChildModel("Help", R.drawable.ic_apollo_new_drug_request__1_))
+                        .addChildModel(new ChildModel("LogOut", R.drawable.ic_apollo_pending))
+
+
+                );
+            }
+
+        }
 
 
 //        listView.addHeaderModel(new HeaderModel("Champs", Color.WHITE, true, R.drawable.ic_menu_champ)
--- app/src/main/java/com/apollopharmacy/vishwam/ui/rider/login/BackSlash.java
+++ app/src/main/java/com/apollopharmacy/vishwam/ui/rider/login/BackSlash.java
@@ -2,10 +2,22 @@ package com.apollopharmacy.vishwam.ui.rider.login;
 
 public class BackSlash {
     public static String removeBackSlashes(String resp) {
-        return resp.replaceAll("\\\\", "");
+        String resp1 = resp.toString()
+                .replace("\"[", "[").replace("]\"", "]")
+                .replace("\\\"{", "{").replace("}\\\"", "}")
+                .replace("\\\\\\\"", "\"");
+        return resp1.replaceAll("\\\\", "");
     }
+//     String responseString = resp.replaceAll("\\\\", "");
+//        if (responseString.contains("\\\\")){
+//            return responseString.replaceAll("\\\\", "");
+//        }else {
+//            return responseString;
+//        }
 
     public static String removeSubString(String res) {
+
+
         if (res.contains("}{"))
             return res.substring(1, res.indexOf("}{") + 1);
         else
