8489d5f6a9d627eeececba2ecca9deb7e3954b00
pc <test@test.com>
2023-05-30 17:36:54 +0530

    .

--- app/src/main/java/com/apollopharmacy/vishwam/ui/home/MainActivity.java
+++ app/src/main/java/com/apollopharmacy/vishwam/ui/home/MainActivity.java
@@ -1578,13 +1578,11 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         if (Preferences.INSTANCE.getAppLevelDesignationApnaRetro().toLowerCase().contains("nodata")||Preferences.INSTANCE.getAppLevelDesignationApnaRetro().toLowerCase().contains("super")){
             listView.addHeaderModel(new HeaderModel("Apna Retro", Color.WHITE, true, R.drawable.ic_menu_champ)
-                    .addChildModel(new ChildModel("Creation", R.drawable.ic_apollo_survey_68__1_))
-                    .addChildModel(new ChildModel("Approval", R.drawable.ic_apollo_survey_68__1_)));
+                    .addChildModel(new ChildModel("Creation", R.drawable.ic_apollo_survey_68__1_)));
 
         }
         else if (Preferences.INSTANCE.getAppLevelDesignationApnaRetro().toLowerCase().contains("exec")||Preferences.INSTANCE.getAppLevelDesignationApnaRetro().toLowerCase().contains("manag")||Preferences.INSTANCE.getAppLevelDesignationApnaRetro().toLowerCase().contains("ceo")||Preferences.INSTANCE.getAppLevelDesignationApnaRetro().toLowerCase().contains("gm")) {
             listView.addHeaderModel(new HeaderModel("Apna Retro", Color.WHITE, true, R.drawable.ic_menu_champ)
-                    .addChildModel(new ChildModel("Creation", R.drawable.ic_apollo_survey_68__1_))
                     .addChildModel(new ChildModel("Approval", R.drawable.ic_apollo_survey_68__1_)));
         }
 
