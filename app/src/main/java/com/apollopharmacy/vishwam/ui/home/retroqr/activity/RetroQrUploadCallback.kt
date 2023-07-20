516e638f8b5758468aec9cd200d8a25ca51a9522
TH_UDAY <test@gmail.com>
2023-07-20 11:37:44 +0530

    .

--- app/src/main/java/com/apollopharmacy/vishwam/ui/home/retroqr/activity/RetroQrUploadCallback.kt
+++ app/src/main/java/com/apollopharmacy/vishwam/ui/home/retroqr/activity/RetroQrUploadCallback.kt
@@ -1,10 +1,14 @@
 package com.apollopharmacy.vishwam.ui.home.retroqr.activity
 
+import android.view.View
 import com.apollopharmacy.vishwam.ui.home.retroqr.activity.model.StoreWiseRackDetails
 
 interface RetroQrUploadCallback {
     fun onClickBackArrow()
     fun onClickSubmit()
+
+    fun imageData(position: Int, imageUrl: String, rackNo: String, qrCode: String,view:View)
+
     fun onSuccessUploadImagesApiCall(message: String)
     fun onFailureUploadImagesApiCall(message: String)
 
