9cb4dd5a80d3c921fd0397747d6cb2b3d444a071
TH_UDAY <test@gmail.com>
2023-07-26 16:29:45 +0530

    pull

--- app/src/main/java/com/apollopharmacy/vishwam/data/ViswamAppApi.kt
+++ app/src/main/java/com/apollopharmacy/vishwam/data/ViswamAppApi.kt
@@ -71,12 +71,12 @@ interface ViswamAppApi {
     ): DeviceDeRegResponse
 
 
-    /*@POST("https://172.16.103.116:8443/mrodvend/APOLLO/Vendor/VALIDATEVENDOR")
-    suspend fun getValidate(@Header("token") token: String, @Body data: CommonRequest): String*/
-
-    @POST("https://viswam.apollopharmacy.org/mprodvend/APOLLO/Vendor/VALIDATEVENDOR")
+    @POST("https://172.16.103.116:8443/mrodvend/APOLLO/Vendor/VALIDATEVENDOR")
     suspend fun getValidate(@Header("token") token: String, @Body data: CommonRequest): String
 
+   /* @POST("https://viswam.apollopharmacy.org/mprodvend/APOLLO/Vendor/VALIDATEVENDOR")
+    suspend fun getValidate(@Header("token") token: String, @Body data: CommonRequest): String
+*/
 
     @GET("https://jsonblob.com/api/jsonBlob/1100710312562409472")
     suspend fun getValidateTest(): ValidateResponse
