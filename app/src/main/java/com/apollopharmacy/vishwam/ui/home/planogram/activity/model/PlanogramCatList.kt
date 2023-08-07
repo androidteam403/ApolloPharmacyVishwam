7cf53ab71817d1653714bc8e55217ca7a42b43ed
pc <test@gmail.com>
2023-08-07 15:49:00 +0530

    push

---
+++ app/src/main/java/com/apollopharmacy/vishwam/ui/home/planogram/activity/model/PlanogramCatList.kt
@@ -0,0 +1,7 @@
+package com.apollopharmacy.vishwam.ui.home.planogram.activity.model
+
+class PlanogramCatList {
+    var categoryList: MutableList<CatList>? = null
+
+    class CatList(var name: String?, var colorCode:String?)
+}
---
+++ app/src/main/java/com/apollopharmacy/vishwam/ui/home/planogram/fragment/PlanogramFragment.kt
@@ -0,0 +1,60 @@
+package com.apollopharmacy.vishwam.ui.home.planogram.fragment
+
+import android.content.Intent
+import androidx.lifecycle.ViewModelProvider
+import com.apollopharmacy.vishwam.R
+import com.apollopharmacy.vishwam.base.BaseFragment
+import com.apollopharmacy.vishwam.databinding.FragmentPlanogramBinding
+import com.apollopharmacy.vishwam.ui.home.MainActivityCallback
+import com.apollopharmacy.vishwam.ui.home.planogram.activity.PlanogramEvaluationActivity
+import com.apollopharmacy.vishwam.ui.home.swach.swachlistmodule.approvelist.ApproveListActivity
+import com.apollopharmacy.vishwam.ui.home.swach.swachlistmodule.previewImage.PreviewImageActivity
+
+class PlanogramFragment : BaseFragment<PlanogramViewModel, FragmentPlanogramBinding>(),
+    PlanogramCallback, MainActivityCallback {
+    override val layoutRes: Int
+        get() = R.layout.fragment_planogram
+
+    override fun retrieveViewModel(): PlanogramViewModel {
+        return ViewModelProvider(this).get(PlanogramViewModel::class.java)
+    }
+
+    override fun setup() {
+        viewBinding.callback=this
+
+    }
+
+    override fun onClickFilterIcon() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onClickSiteIdIcon() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onClickQcFilterIcon() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onSelectApprovedFragment(listSize: String?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onSelectRejectedFragment() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onSelectPendingFragment() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onClickSpinnerLayout() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onClickContinue() {
+        val intent = Intent(context, PlanogramEvaluationActivity::class.java)
+        startActivity(intent)
+        activity?.overridePendingTransition(R.anim.slide_from_right, R.anim.slide_to_left)
+    }
+}
